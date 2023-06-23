e97d0e147c51a174051898ca332f7f2a45d33b3c
Max <sibo_jian_29903549@163.com>
2023-06-23 23:24:09 +0800

    Add SBContainerViewController.swift file

---
+++ Sources/Core/SBContainerViewController.swift
@@ -0,0 +1,14 @@
+//
+//  SBContainerViewController.swift
+//  SBNavigationController
+//
+//  Created by Max on 2023/6/23.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+public class SBContainerViewController: UIViewController {}
+
+#endif
