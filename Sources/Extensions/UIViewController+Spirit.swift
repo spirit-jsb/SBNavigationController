0b8c04fef463ed6bf5a7f89ef6f202560e3fc0c3
Max <sibo_jian_29903549@163.com>
2023-06-23 23:24:49 +0800

    Add UIViewController+Spirit.swift file

---
+++ Sources/Extensions/UIViewController+Spirit.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Spirit.swift
+//  SBNavigationController
+//
+//  Created by Max on 2023/6/23.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+extension UIViewController {
+    @objc
+    open var navigationBarClass: AnyClass? {
+        return nil
+    }
+}
+
+#endif
