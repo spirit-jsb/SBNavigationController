c08fdd3b1e578fae9c244bd92299ce6756443b8c
Max <sibo_jian_29903549@163.com>
2023-06-23 22:12:36 +0800

    Add Spirit.swift file

---
+++ Sources/Core/Spirit.swift
@@ -0,0 +1,46 @@
+//
+//  Spirit.swift
+//  SBNavigationController
+//
+//  Created by Max on 2023/6/23.
+//
+
+#if canImport(UIKit)
+
+import UIKit
+
+private var disablesInteractivePopKey: Void?
+
+public struct SpiritWrapper<Base> {
+    public let base: Base
+
+    public init(_ base: Base) {
+        self.base = base
+    }
+}
+
+public protocol SpiritCompatible: AnyObject {}
+
+public extension SpiritCompatible {
+    var sb: SpiritWrapper<Self> {
+        get {
+            return SpiritWrapper(self)
+        }
+        set {}
+    }
+}
+
+extension UIViewController: SpiritCompatible {}
+
+public extension SpiritWrapper where Base: UIViewController {
+    var disablesInteractivePop: Bool! {
+        get {
+            return getAssociatedObject(self.base, &disablesInteractivePopKey)
+        }
+        set {
+            setAssociatedObject(self.base, &disablesInteractivePopKey, newValue)
+        }
+    }
+}
+
+#endif
