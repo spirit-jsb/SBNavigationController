103387ea12ab4a82272e66669b4bb4e99a970310
Max <sibo_jian_29903549@163.com>
2023-06-23 21:46:18 +0800

    Initial SBNavigationController project

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  SBNavigationController-Demo
+//
+//  Created by Max on 2023/6/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
---
+++ Sources/SBNavigationController.h
@@ -0,0 +1,18 @@
+//
+//  SBNavigationController.h
+//  SBNavigationController
+//
+//  Created by Max on 2023/6/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SBNavigationController.
+FOUNDATION_EXPORT double SBNavigationControllerVersionNumber;
+
+//! Project version string for SBNavigationController.
+FOUNDATION_EXPORT const unsigned char SBNavigationControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SBNavigationController/PublicHeader.h>
+
+
